cf2b9814a02fc2abe32abdd58ffb963a279613c8
Flare <flarusnova@gmail.com>
2022-07-20 15:44:36 -0400

    Checkpoint 5 finally

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -5,7 +5,11 @@ public class gameManager : MonoBehaviour
 {
     bool gameEndBool = false;
     public float restartDelay = 1f;
+    public GameObject completeLevelUI;
 
+    public void CompleteLevel() {
+        completeLevelUI.SetActive(true);
+    }
     public void EndGame()  {
         if (!gameEndBool)    {
             gameEndBool = true;
